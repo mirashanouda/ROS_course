ed7cade28b7b18c555129c94aa9b1ed2709ba63d
Mira Shanouda <mira.shanouda>
2020-08-13 03:28:41 +0200

    Task 6

---
+++ Day2/tasks/src/cmake/CMakeFiles/c++file.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c++file.dir/main.cpp.o"
+  "c++file.pdb"
+  "c++file"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c++file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
