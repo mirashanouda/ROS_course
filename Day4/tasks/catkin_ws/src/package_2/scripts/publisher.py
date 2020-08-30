1065d1fa2ee0a2cb09f1924bfc4f8218da814a2e
Mira Shanouda <mira.shanouda>
2020-08-30 16:47:22 +0200

    D4: T1: Write publish and subscribe count from 0 until the user terminate (c++/py)

--- Day4/tasks/catkin_ws/src/package_2/scripts/publisher.py
+++ Day4/tasks/catkin_ws/src/package_2/scripts/publisher.py
@@ -3,27 +3,28 @@
 #Full documentation: http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
 
 import rospy #importing ros library in python
-from std_msgs.msg import String #it can be int or float
+from std_msgs.msg import Int32 #it can be int or float
 
 
 def talker():
     #creating the node:
-    rospy.init_node("pub_node", anonymous=True) 
+    rospy.init_node("pub_counter_node", anonymous=True) 
     #In ROS, nodes are named uniqely. If 2 nodes with the same name are lunched, the previous one is kicked off.
     #ananymos=True makes rospy choose a uniqe name for each node so that multiple nodes can run simultaneously. 
 
-    pub = rospy.Publisher("/myTopic", String, queue_size=10) 
+    pub = rospy.Publisher("/countTopic", Int32, queue_size=10) 
     #the publisher topic should be exactly the same as the subscription topic
     #pub is a variable, (name of the topic, .., bufer: stadered 10 but it can be 30)
 
     rate = rospy.Rate(1)
-    #hz: every 1sec. a message will be sent
+    #hz: every 1 sec. a message will be sent
 
+    count = 0 
     #this loop is terminated with ctrl C
     while not rospy.is_shutdown(): 
-        hello_str = "Hello World"
-        rospy.loginfo(hello_str) #to print
-        pub.publish(hello_str) #to publish
+        count = count + 1
+        rospy.loginfo(count) #to print
+        pub.publish(count) #to publish
         rate.sleep() #this makes the loop works until ctrl C
 
 #starting main function:    
---
+++ Day4/tasks/catkin_ws/src/package_2/scripts/subscriber.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+#Full documentation: http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
+
+
+import rospy #importing ros library in python
+from std_msgs.msg import Int32 #it can be int or float
+
+def callback(num):
+    rospy.loginfo("The counter: %s", num.data)
+
+def listener():
+    #creating the node:
+
+    rospy.init_node("sub_counter_node", anonymous=True) # to  the node takes a uniqe name in the machine so even if some nodes have the same name, the program doesn't work well
+    #In ROS, nodes are named uniqely. If 2 nodes with the same name are lunched, the previous one is kicked off.
+    #ananymos=True makes rospy choose a uniqe name for each node so that multiple nodes can run simultaneously. 
+    
+    rospy.Subscriber("/countTopic", Int32, callback) #callback is the function that will be listened
+    #the publisher topic should be exactly the same as the subscription topic
+    rospy.spin() 
+    #Blocks until ROS node is shutdown. 
+
+
+if __name__ == '__main__':
+    listener()
+
+
